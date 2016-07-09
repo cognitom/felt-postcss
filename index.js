3cda4ea16be8773ae88e63708d502fbaa3519f9d
Tsutomu Kawamura <cognitom@gmail.com>
2016-07-10 01:55:47 +0900

    Supports sourcemaps

--- index.js
+++ index.js
@@ -8,20 +8,26 @@ const
 /** default config file name */
 const defaultConfigFileName = 'postcss.config.js'
 
-module.exports = function(opts) {
-  if (typeof opts == 'string' || !opts) {
+module.exports = function(config) {
+  if (typeof config == 'string' || !config) {
     try {
       const
         root = process.cwd(),
-        configFile = path.join(root, opts || defaultConfigFileName)
-      opts = require(configFile)
+        configFile = path.join(root, config || defaultConfigFileName)
+      config = require(configFile)
     } catch(e) {
-      opts = require(path.join(__dirname, defaultConfigFileName))
+      config = require(path.join(__dirname, defaultConfigFileName))
     }
   }
   return co.wrap(function* (from, to){
-    const css = yield fsp.readFile(from, 'utf8')
-    yield postcss(opts).process(css, { from, to })
+    const
+      options = Object.assign({}, config.options, { from, to }),
+      css = yield fsp.readFile(from, 'utf8'),
+      result = yield postcss(config.plugins).process(css, options)
+
+    yield fsp.writeFile(to, result.css)
+    if (result.map)
+      yield fsp.writeFile(`${ to }.map`, result.map)
 
     // TODO: return dependencies
   })
--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,12 @@ const
   postcssImport = require('postcss-import'),
   autoprefixer = require('autoprefixer')
 
-module.exports = [
-  postcssImport(),
-  autoprefixer()
-]
+module.exports = {
+  plugins: [
+    postcssImport(),
+    autoprefixer()
+  ],
+  options: {
+    map: { inline: false }
+  }
+}
